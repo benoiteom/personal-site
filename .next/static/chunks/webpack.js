b6cef1c81b6d010a845dfe548041e36b0315ed77
Benoit Ortalo-Magne <benoiteom@gmail.com>
2021-01-12 16:03:08 -0700

    fix: details styling

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "18a2b14245d26a4768e1";
+/******/ 	var hotCurrentHash = "7dcbf74924096324542a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
